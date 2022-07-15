b9a4a8fcce5719123a44cd020fcd556abee8d771
rakibul-wdp <rakibul.wdp@gmail.com>
2022-07-15 22:00:13 +0600

    add signout method

--- src/Pages/Drawing/Drawing.js
+++ src/Pages/Drawing/Drawing.js
@@ -1,9 +1,23 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Drawing = () => {
+  const [user] = useAuthState(auth);
+
+  const logout = () => {
+    signOut(auth);
+    localStorage.removeItem('accessToken');
+  };
   return (
     <div>
       <h2>Drawing page</h2>
+      {user && (
+        <button className='btn btn-ghost' onClick={logout}>
+          Sign Out
+        </button>
+      )}
     </div>
   );
 };
